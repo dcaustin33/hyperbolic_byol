6755eab87b940383542a3c0ec2724f02f3249538
dcaustin33 <dcaustin33@gmail.com>
2022-05-24 19:44:13 -0400

    Updated with hyperbolic equivalent, distance function and coarse hierarchical labels

--- BYOL.py
+++ BYOL.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[1]:
 
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import ResNets
+import resnets
 import torch.nn.functional as F
 import torchvision
     
@@ -15,14 +15,27 @@ import copy
 import torch
 import torch.nn.functional as F
 
+from geoopt.manifolds.stereographic import math as gmath
 
-# In[6]:
 
+# In[2]:
+
+
+from torchsummary import summary
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def ResNet18(cifar = True):
+def ResNet18(cifar = True, classification = False):
     model = torchvision.models.resnet18()
+    if not classification:
+        model.fc = nn.Identity()
+    if cifar:
+        model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
+        model.maxpool = nn.Identity()
+    return model
+
+def ResNet34(cifar = True):
+    model = torchvision.models.resnet34()
     model.fc = nn.Identity()
     if cifar:
         model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
@@ -30,8 +43,10 @@ def ResNet18(cifar = True):
     return model
 
 
+# In[3]:
 
-def byol_loss_func(p: torch.Tensor, z: torch.Tensor, simplified: bool = True):
+
+def euclidean_byol_loss_func(p: torch.Tensor, z: torch.Tensor, simplified: bool = True):
     """Computes BYOL's loss given batch of predicted features p and projected momentum features z.
     Args:
         p (torch.Tensor): NxD Tensor containing predicted features from view 1
@@ -43,6 +58,12 @@ def byol_loss_func(p: torch.Tensor, z: torch.Tensor, simplified: bool = True):
     return 2 - 2 * F.cosine_similarity(p, z.detach(), dim=-1).mean()
 
 
+def hyperbolic_distance(x: torch.Tensor, y: torch.Tensor, k: torch.Tensor, keepdim=False, dim=-1):
+    """
+    Find the geodisic between x and y for the given curvature
+    """
+    return gmath.dist(x, y, k = k, keepdim=False, dim=-1)
+
 def update_target_params(online_params, target_params, tau):
 
     #update the backbone first
@@ -50,17 +71,19 @@ def update_target_params(online_params, target_params, tau):
         mp.data = tau * mp.data + (1 - tau) * op.data
 
 
-# In[ ]:
+# In[4]:
 
 
-class BYOL_module(nn.Module):
+
+class euclidean_BYOL_module(nn.Module):
     
-    def __init__(self, input_size = (3, 32, 32), classes = 100, projection_size = 256, projection_hidden_size = 4096):
+    def __init__(self, input_size = (3, 32, 32), classes = 100, projection_size = 256, projection_hidden_size = 4096, coarse_classes = 20):
         super().__init__()
         self.network = ResNet18()
         example = torch.ones(input_size)
         self.feature_dim = (self.network(example.unsqueeze(0))).shape[1]
         self.classifier = nn.Linear(self.feature_dim, classes)
+        self.coarse_classifier = nn.Linear(self.feature_dim, coarse_classes)
         
         self.projector = nn.Sequential(
             nn.Linear(self.feature_dim, projection_hidden_size),
@@ -79,19 +102,91 @@ class BYOL_module(nn.Module):
             representation = self.network(x)
             z = self.projector(representation)
         logits = self.classifier(representation.detach())
+        coarse_logits = self.coarse_classifier(representation.detach())
         out = {'Representation': representation,
               "logits": logits,
+              "coarse_logits": coarse_logits,
               "z": z}    
         return out
     
     def forward(self, x):
         representation = self.network(x)
         logits = self.classifier(representation.detach())
+        coarse_logits = self.coarse_classifier(representation.detach())
         z = self.projector(representation)
         p = self.predictor(z)
         
         out = {'Representation': representation,
               "logits": logits,
+              "coarse_logits": coarse_logits,
               "z": z,
               "p": p} 
         return out
+
+
+# In[39]:
+
+
+import mobius
+
+class hyperbolic_BYOL_module(nn.Module):
+    
+    def __init__(self, input_size = (3, 32, 32), classes = 100, projection_size = 256, projection_hidden_size = 4096, coarse_classes = 20):
+        super().__init__()
+        self.network = ResNet18()
+        example = torch.ones(input_size)
+        self.feature_dim = (self.network(example.unsqueeze(0))).shape[1]
+        self.classifier = nn.Linear(self.feature_dim, classes)
+        self.coarse_classifier = nn.Linear(self.feature_dim, coarse_classes)
+        
+        self.projector = nn.Sequential(
+            mobius.MobiusLinear(self.feature_dim, projection_hidden_size, hyperbolic_input = False, nonlin = nn.ReLU(), fp64_hyper = False),
+            mobius.MobiusLinear(projection_hidden_size, projection_size, nonlin = nn.ReLU(), fp64_hyper = False))
+        
+        self.predictor = nn.Sequential(
+            mobius.MobiusLinear(projection_size, projection_hidden_size, nonlin = nn.ReLU(), fp64_hyper = False),
+            mobius.MobiusLinear(projection_hidden_size, projection_size, nonlin = nn.ReLU(), fp64_hyper = False))
+        
+
+    def momentum_forward(self, x):
+        with torch.no_grad():
+            representation = self.network(x)
+            z = self.projector(representation)
+        logits = self.classifier(representation.detach())
+        coarse_logits = self.coarse_classifier(representation.detach())
+        out = {'Representation': representation,
+              "logits": logits,
+              "coarse_logits": coarse_logits,
+              "z": z}    
+        return out
+    
+    def forward(self, x):
+        representation = self.network(x)
+        logits = self.classifier(representation.detach())
+        coarse_logits = self.coarse_classifier(representation.detach())
+        z = self.projector(representation)
+        p = self.predictor(z)
+        
+        out = {'Representation': representation,
+              "logits": logits,
+              "coarse_logits": coarse_logits,
+              "z": z,
+              "p": p} 
+        return out
+
+
+# In[45]:
+
+
+hbyol = hyperbolic_BYOL_module()
+ex = torch.randn(10, 3, 32, 32)
+out = hbyol(ex)
+x = torch.zeros(out['p'].shape)
+dist = hyperbolic_distance(out['z'], out['p'], torch.Tensor([-1]), keepdim=False, dim=-1)
+
+
+# In[ ]:
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,99 @@
+import torch.nn.functional as F
+import torch
+import time
+from pretrain_dataloader import *
+import pickle
+
+def update_target_params(online_params, target_params, tau):
+
+    #update the backbone first
+    for op, mp in zip(online_params, target_params):
+        mp.data = tau * mp.data + (1 - tau) * op.data
+
+def set_params_equal(online_params, target_params):
+
+    #update the backbone first
+    for op, mp in zip(online_params, target_params):
+        mp.data = op.data
+    
+def byol_loss_func(p: torch.Tensor, z: torch.Tensor, simplified: bool = True) -> torch.Tensor:
+    return 2 - 2 * F.cosine_similarity(p, z.detach(), dim=-1).mean()
+
+
+def prepare_cifar_train_loader(args):
+    transform = [
+        prepare_transform(args.dataset, **kwargs) for kwargs in args.transform_kwargs
+    ]
+
+    transform = prepare_n_crop_transform(transform, num_crops_per_aug=args.num_crops_per_aug)
+
+    train_dataset = prepare_datasets(
+        args.dataset,
+        transform,
+        no_labels=False,
+    )
+    train_loader = prepare_dataloader(
+        train_dataset, batch_size=args.batch_size, num_workers=args.num_workers
+    )
+    return train_loader
+
+
+class args:
+    def __init__(self):
+        return
+        
+
+def return_default_args(args = args()):
+    args.dataset = 'cifar100'
+    args.transform_kwargs=[{'brightness': 0.4, 'contrast': 0.4, 'saturation': 0.2, 'hue': 0.1, 'color_jitter_prob': 0.8, 'gray_scale_prob': 0.2, 'horizontal_flip_prob': 0.5, 'gaussian_prob': 1.0, 'solarization_prob': 0.0, 'crop_size': 32, 'min_scale': 0.08, 'max_scale': 1.0}, {'brightness': 0.4, 'contrast': 0.4, 'saturation': 0.2, 'hue': 0.1, 'color_jitter_prob': 0.8, 'gray_scale_prob': 0.2, 'horizontal_flip_prob': 0.5, 'gaussian_prob': 0.1, 'solarization_prob': 0.2, 'crop_size': 32, 'min_scale': 0.08, 'max_scale': 1.0}]
+    # asymmetric augmentations
+    args.num_crops_per_aug = [1, 1]
+    args.batch_size = 256
+    args.num_workers = 4
+    
+    args.optimizer = 'LARS'
+    args.lr = .001
+    args.weight_decay = 1.5e-6
+    args.momentum = .9
+    args.classifier_lr = .01
+    args.classifier_weight_decay = 0
+    args.epochs = 200
+    args.warmup_epochs = 10
+    args.steps = int((50000/args.batch_size) * args.epochs)
+    args.warmup_steps = int((50000/args.batch_size) * args.warmup_epochs)
+    return args
+
+
+def fine_to_coarse():
+    """
+    function to map the fine labels to the coarser labels for cifar-100
+    """
+    superclass = [['beaver', 'dolphin', 'otter', 'seal', 'whale'],
+                  ['aquarium_fish', 'flatfish', 'ray', 'shark', 'trout'],
+                  ['orchid', 'poppy', 'rose', 'sunflower', 'tulip'],
+                  ['bottle', 'bowl', 'can', 'cup', 'plate'],
+                  ['apple', 'mushroom', 'orange', 'pear', 'sweet_pepper'],
+                  ['clock', 'keyboard', 'lamp', 'telephone', 'television'],
+                  ['bed', 'chair', 'couch', 'table', 'wardrobe'],
+                  ['bee', 'beetle', 'butterfly', 'caterpillar', 'cockroach'],
+                  ['bear', 'leopard', 'lion', 'tiger', 'wolf'],
+                  ['bridge', 'castle', 'house', 'road', 'skyscraper'],
+                  ['cloud', 'forest', 'mountain', 'plain', 'sea'],
+                  ['camel', 'cattle', 'chimpanzee', 'elephant', 'kangaroo'],
+                  ['fox', 'porcupine', 'possum', 'raccoon', 'skunk'],
+                  ['crab', 'lobster', 'snail', 'spider', 'worm'],
+                  ['baby', 'boy', 'girl', 'man', 'woman'],
+                  ['crocodile', 'dinosaur', 'lizard', 'snake', 'turtle'],
+                  ['hamster', 'mouse', 'rabbit', 'shrew', 'squirrel'],
+                  ['maple_tree', 'oak_tree', 'palm_tree', 'pine_tree', 'willow_tree'],
+                  ['bicycle', 'bus', 'motorcycle', 'pickup_truck', 'train'],
+                  ['lawn_mower', 'rocket', 'streetcar', 'tank', 'tractor']]
+
+    index = 2
+    fine_to_coarse = {}
+    for i, label in enumerate(pickle.load(open('../datasets/cifar100/train/cifar-100-python/meta', 'rb'))['fine_label_names']):
+        for k, coarse in enumerate(superclass):
+            for clabel in coarse:
+                if label == clabel:
+                    fine_to_coarse[i] = k
+    return fine_to_coarse
